e0381b5194bd3bee69787e1e228b0da93bb7bd43
LeoMezba <csmezba@gmail.com>
2020-12-15 15:22:53 +0600

    Outputing Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,14 +1,33 @@
+import { object } from 'prop-types';
 import React from 'react';
 import Classes from './Burger.module.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const burger = (props) => {
 
+    let transformedIngredients = Object.keys( props.ingredients) // convert object into array
+    .map( igKey => {
+        // console.log (props.ingredients[igKey]); // Grab Object Properties Value
+        return [...Array(props.ingredients[igKey])]
+        .map( (_, i) => {
+            return <BurgerIngredients key={igKey + i} type={igKey} />
+        });
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    });
+
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0)
+    {
+        transformedIngredients = <p>Please Select Some Ingredients</p>
+    }
+
     return (
         <div className={Classes.Burger}>
             <BurgerIngredients type="Bread-top"/>
-            <BurgerIngredients type="Cheese"/>
-            <BurgerIngredients type="Meat"/>
+            {transformedIngredients}
             <BurgerIngredients type="Bread-Bottom"/>
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,21 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aox';
 
 class burger extends Component {
+
+    state = {
+        ingredients : {
+            Meat : 0,
+            Salad : 1,
+            Cheese : 2,
+            Bacon : 0
+        }
+    }
+
     render () {
         return(
             <Aux>
                 <div>
-                   <Burger />
+                   <Burger ingredients={this.state.ingredients}/>
                 </div>
             </Aux>
         )
